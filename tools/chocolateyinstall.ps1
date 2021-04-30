af1c212246295746c7414879d8ff81f2f3bade35
Ryan Bartling <ryan.bartling@thinkdeca.com>
2021-04-30 12:47:27 -0700

    fix: Point to deca's internal dropbox URL

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ $ErrorActionPreference = 'Stop';
 
 $packageName= 'vcpython27'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://download.microsoft.com/download/7/9/6/796EF2E4-801B-4FC4-AB28-B59FBF6D907B/VCForPython27.msi'
+$url        = 'https://www.dropbox.com/s/blc4xk29p6i90uy/VCForPython27.msi?dl=1'
 
 $packageArgs = @{
   packageName   = $packageName
